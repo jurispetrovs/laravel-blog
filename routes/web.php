c2b56ab9f6bdd22edf36c48ff3de6a77787ce8c8
Juris Petrovs <jura-petrov@inbox.lv>
2020-11-25 13:54:53 +0200

    Added middleware

--- routes/web.php
+++ routes/web.php
@@ -1,18 +1,6 @@
 <?php
 
-use Illuminate\Support\Facades\Auth;
 use Illuminate\Support\Facades\Route;
-
-/*
-|--------------------------------------------------------------------------
-| Web Routes
-|--------------------------------------------------------------------------
-|
-| Here is where you can register web routes for your application. These
-| routes are loaded by the RouteServiceProvider within a group which
-| contains the "web" middleware group. Now create something great!
-|
-*/
 
 Route::get('/', function () {
     return view('welcome');
@@ -22,4 +10,9 @@ Auth::routes();
 
 Route::get('/home', [App\Http\Controllers\HomeController::class, 'index'])->name('home');
 
-Route::resource('articles', 'ArticlesController');
+Route::resource('articles', 'ArticlesController')
+    ->middleware(['auth'])
+    ->except(['index', 'show']);
+
+Route::get('articles', 'ArticlesController@index')->name('articles.index');
+Route::get('articles/{article}', 'ArticlesController@show')->name('articles.show');
